b60f40d76cf419f4dc2296cddf278cc5963b0835
stephany-ribeiro-dev <stephanysr@hotmail.com>
2025-11-27 23:42:07 -0300

    feat: spring.io initializer package structure

---
+++ src/main/java/br/com/desafios/candidatura/CandidaturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafios.candidatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidaturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafios/candidatura/CandidaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafios.candidatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
