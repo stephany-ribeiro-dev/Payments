c988e8b0eb3f433ade4e531193fc763c5fe3e066
stephany-ribeiro-dev <stephanysr@hotmail.com>
2025-11-28 01:50:38 -0300

    feat: Convert single-file component to basic MVC structure

---
+++ src/main/java/br/com/desafios/candidatura/service/PaymentsService.java
@@ -0,0 +1,53 @@
+package br.com.desafios.candidatura.service;
+
+import br.com.desafios.candidatura.dto.PaymentCreditDTO;
+import br.com.desafios.candidatura.dto.PaymentDebitDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentsService {
+
+    public void debitCard(PaymentDebitDTO paymentDebitDTO){
+        System.out.println("Processando cartao de debito...");
+        if( !validateDebitCard(paymentDebitDTO)) {
+            System.out.println("Falha na validacao do cartao de debito.");
+            return;
+        }
+        processDebitCardPayment();
+    }
+
+    public void creditCard(PaymentCreditDTO paymentCreditDTO){
+        System.out.println("Processando cartao de credito...");
+        if (!validateCreditCard(paymentCreditDTO)) {
+            System.out.println("Falha na validacao do cartao de credito.");
+            return;
+        }
+        processCreditCardPayment();
+    }
+
+    private boolean validateDebitCard(PaymentDebitDTO paymentDebitDTO){
+        if (paymentDebitDTO.getAccountNumber() == null || paymentDebitDTO.getBankCode() == null) {
+            System.out.println("Dados do cartao incompletos.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateCreditCard(PaymentCreditDTO paymentCreditDTO){
+        if (paymentCreditDTO.getCardNumber() == null || paymentCreditDTO.getExpirationDate() == null
+                || paymentCreditDTO.getCVV() == null) {
+            System.out.println("Dados do cartao incompletos.");
+            return false;
+        }
+        return true;
+    }
+
+    private void processDebitCardPayment() {
+        System.out.println("Processando pagamento com cartao de debito...");
+    }
+
+    private void processCreditCardPayment() {
+        System.out.println("Processando pagamento com cartao de credito...");
+    }
+
+}
